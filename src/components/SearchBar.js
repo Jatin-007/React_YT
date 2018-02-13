f399f394cc884148c34894b9c160c157f018c082
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-02-12 20:20:53 -0800

    Nothing fancy

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {term : '' };
     }
 
     render () {
         return (
             <div>
-                <input onChange={(event)=> console.log(event.target.value)}/>
+                <input onChange={event=> this.setState({ term: event.target.value })}/>
+                <br/>
+
+                Output: {this.state.term}
             </div>
         )
     }
