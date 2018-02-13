0dedba5144207c1aac37238bd99b53a5e1abec09
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-02-12 19:34:31 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './components/SearchBar';
+
+//const API_KEY = ' AIzaSyAlMqNkwfePYB3HBm24bTRpNpBth6KC0pE ';
+
+const App = ()=> {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'; 
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <div>
+                <input onChange={(event)=> console.log(event.target.value)}/>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
