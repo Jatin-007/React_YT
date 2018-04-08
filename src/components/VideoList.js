4f4ffb78b09c58e3a43f10f5e85271af060e20b4
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-04-08 02:30:50 -0700

    Working with videoList

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,10 +2,10 @@ import React from 'react';
 import VideoListItem from './VideoListItem';
 
 const VideoList = (props)=> {
-    const videoItems = props.videos.map((video)=>{
-        return <VideoListItem video={video}/>
-    });
-
+    const videoItems = props.videos.map((video)=> {
+        return <VideoListItem video = {video}/>
+    })
+    
     return (
         <ul className="col-md-4 list-group">
             {videoItems}
@@ -14,4 +14,3 @@ const VideoList = (props)=> {
 };
 
 export default VideoList;
-
